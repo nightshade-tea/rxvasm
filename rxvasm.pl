70c7648ec392132a7d74fc537cf11c5f8167fc51
phfr24 <phfr24@inf.ufpr.br>
2025-05-01 12:58:18 -0300

    unify types and opcodes into a single hash

--- rxvasm.pl
+++ rxvasm.pl
@@ -1,20 +1,59 @@
 use strict;
 use warnings;
 
-my %opcode = (
-    brzr => 0b0000,
-    ji   => 0b0001,
-    ld   => 0b0010,
-    st   => 0b0011,
-    addi => 0b0100,
-    not  => 0b1000,
-    and  => 0b1001,
-    or   => 0b1010,
-    xor  => 0b1011,
-    add  => 0b1100,
-    sub  => 0b1101,
-    slr  => 0b1110,
-    srr  => 0b1111
+my %instr = (
+    brzr => {
+             opcode => 0b0000,
+             type => 'r'
+            },
+    ji   => {
+             opcode => 0b0001,
+             type => 'r'
+            },
+    ld   => {
+             opcode => 0b0010,
+             type => 'r'
+            },
+    st   => {
+             opcode => 0b0011,
+             type => 'r'
+            },
+    addi => {
+             opcode => 0b0100,
+             type => 'r'
+            },
+    not  => {
+             opcode => 0b1000,
+             type => 'r'
+            },
+    and  => {
+             opcode => 0b1001,
+             type => 'r'
+            },
+    or   => {
+             opcode => 0b1010,
+             type => 'r'
+            },
+    xor  => {
+             opcode => 0b1011,
+             type => 'r'
+            },
+    add  => {
+             opcode => 0b1100,
+             type => 'r'
+            },
+    sub  => {
+             opcode => 0b1101,
+             type => 'r'
+            },
+    slr  => {
+             opcode => 0b1110,
+             type => 'r'
+            },
+    srr  => {
+             opcode => 0b1111,
+             type => 'r'
+            }
 );
 
 # parses an r-format instruction string and returns (op, ra, rb)
