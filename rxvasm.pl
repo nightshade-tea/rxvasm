d9a560ccb638c1f082aabbd0662684d0fab6f6e3
phfr24 <phfr24@inf.ufpr.br>
2025-05-01 18:06:14 -0300

    update rxvasm.pl

--- rxvasm.pl
+++ rxvasm.pl
@@ -1,7 +1,9 @@
+#!/usr/bin/perl
+
 use strict;
 use warnings;
 
-require './rxvdef.pl' or die "failed to load rxvdef.pl: $@ $!";
+require './rxvdef.pl' or die "Failed to load rxvdef.pl: $@ $!";
 
 # parses an r-format instruction string and returns (op, ra, rb)
 sub parse_r {
@@ -17,4 +19,48 @@ sub parse_i {
     my ($op, $imm) = $instr =~ /([a-z]{2,4})\s+(-?\w+)/;
 
     return ($op, $imm);
+}
+
+die "usage: perl rxvasm.pl <program.s> <out.bin>" if @ARGV != 2;
+
+open my $in, "<", $ARGV[0] or die "Failed to open $ARGV[0]: $!";
+
+my @program;
+
+while (<$in>) {
+    chomp;
+    s/^\s+|\s+$//g; # trim whitespaces
+    s/\s*;.*$//; # strip comments
+    next if /^$/; # skip empty lines
+    push @program, $_;
+}
+
+my @instruction;
+my %label;
+my $addr = 0;
+
+foreach (@program) {
+    my $l;
+
+    if (s/^(\w+):\s*//) {
+        $l = $1;
+        $label{$l} = $addr;
+    }
+
+    unless (/^$/) {
+        push @instruction, $_;
+        $addr++;
+    }
+}
+
+my @binary;
+
+print "$_\n" foreach (@instruction);
+
+foreach (sort keys %label) {
+    print "label=";
+    print;
+    print " addr=";
+    print $label{$_};
+    print "\n";
 }
