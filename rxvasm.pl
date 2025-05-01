09351237f3b8e42ceae8f45e173d46c6b34d821f
phfr24 <phfr24@inf.ufpr.br>
2025-05-01 15:44:56 -0300

    refactor rxvasm.pl: keep things more readable
    also remove error handling that will shoot me on the foot in the future

--- rxvasm.pl
+++ rxvasm.pl
@@ -5,16 +5,16 @@ require './rxvdef.pl' or die "failed to load rxvdef.pl: $@ $!";
 
 # parses an r-format instruction string and returns (op, ra, rb)
 sub parse_r {
-    my $instr = shift or die;
+    my $instr = shift;
+    my ($op, $ra, $rb) = $instr =~ /([a-z]{2,4})\s+(r[0-3]),\s+(r[0-3])/;
 
-    return ($instr =~ /([a-z]{2,4})\s+(r[0-3]),\s+(r[0-3])/
-        or die "invalid instruction format: $instr\n");
+    return ($op, $ra, $rb);
 }
 
 # parses an i-format instruction string and returns (op, imm)
 sub parse_i {
-    my $instr = shift or die;
+    my $instr = shift;
+    my ($op, $imm) = $instr =~ /([a-z]{2,4})\s+(-?\w+)/;
 
-    return ($instr =~ /([a-z]{2,4})\s+(-?\w+)/
-        or die "invalid instruction format: $instr\n");
+    return ($op, $imm);
 }
