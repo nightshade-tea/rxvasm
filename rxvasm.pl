2c906532740ae958b8274784cb47cb03a2de75a9
phfr24 <phfr24@inf.ufpr.br>
2025-05-01 21:28:16 -0300

    add mem_size

--- rxvasm.pl
+++ rxvasm.pl
@@ -7,7 +7,7 @@ require './rxvdef.pl' or die "Failed to load rxvdef.pl: $@ $!";
 
 # parses an r-format instruction string and returns (op, ra, rb)
 sub parse_r {
-    my $instr = shift;
+    my $instr = shift or return;
     my ($op, $ra, $rb) = $instr =~ /([a-z]{2,4})\s+(r[0-3]),\s+(r[0-3])/;
 
     return ($op, $ra, $rb);
@@ -15,10 +15,29 @@ sub parse_r {
 
 # parses an i-format instruction string and returns (op, imm)
 sub parse_i {
-    my $instr = shift;
+    my $instr = shift or return;
     my ($op, $imm) = $instr =~ /([a-z]{2,4})\s+(-?\w+)/;
 
     return ($op, $imm);
+}
+
+# given a trimmed and stripped line (no labels or comments), returns the size
+# in bytes that the instruction or directive needs to be written to the binary
+sub mem_size {
+    my $line = shift or return;
+    my ($dir) = $line =~ /^\.(\w+)/ or return 1;
+
+    if ($rxvdef::directive{$dir}{type} eq 'bits') {
+        my @ops = split /\s+/, $line;
+        return (scalar @ops - 1) * $rxvdef::directive{$dir}{bytes_per_op};
+    }
+
+    elsif ($rxvdef::directive{$dir}{type} eq 'space') {
+        my ($size) = $line =~ /^\.space\s+(\w+)/;
+        return $size;
+    }
+
+    return 0;
 }
 
 die "usage: perl rxvasm.pl <program.s> <out.bin>" if @ARGV != 2;
@@ -35,7 +54,7 @@ while (<$in>) {
     push @program, $_;
 }
 
-my @instruction;
+my @instructions;
 my %label;
 my $addr = 0;
 
@@ -48,14 +67,14 @@ foreach (@program) {
     }
 
     unless (/^$/) {
-        push @instruction, $_;
+        push @instructions, $_;
         $addr++;
     }
 }
 
 my @binary;
 
-print "$_\n" foreach (@instruction);
+print "$_\n" foreach (@instructions);
 
 foreach (sort keys %label) {
     print "label=";
