a9c0a4602249527c8fd994f4a311ae0044df45a8
phfr24 <phfr24@inf.ufpr.br>
2025-05-02 19:11:53 -0300

    refactor rxvasm.pl: add comments and organize

--- rxvasm.pl
+++ rxvasm.pl
@@ -3,63 +3,80 @@
 use strict;
 use warnings;
 
-# TODO: array of imports, iterate requiring
+# import required modules for definitions, encoding, and label extraction
+require $_ foreach ('./rxvdef.pl', './rxvencode.pl', './rxvlabel.pl');
 
-require './rxvdef.pl' or die "Failed to load rxvdef.pl: $@ $!";
-require './rxvencode.pl' or die "Failed to load rxvencode.pl: $@ $!";
-require './rxvlabel.pl' or die "Failed to load rxvlabel.pl: $@ $!";
-
-# reads from a file handle, strips comments, excess whitespace and empty lines,
-# then returns a reference to an array of the resulting program lines
+# strips comments, excess whitespace and empty lines, then returns an arrayref
+# to the resulting program lines
 sub read_program {
+    # args:
+    # $file is a file handle to a program
     my ($file) = @_;
-
     my @program;
 
     while (<$file>) {
-        chomp;
-        s/^\s+|\s+$//g; # trim whitespaces
-        s/\s*;.*$//; # strip comments
-        next if /^$/; # skip empty lines
+        chomp;              # remove trailing newline
+        s/^\s+|\s+$//g;     # trim leading and trailing whitespace
+        s/\s*;.*$//;        # remove comments
+        next if /^$/;       # skip empty lines
         push @program, $_;
     }
 
+    # return:
+    # an arrayref to @program, which is an array of instructions, directives
+    # and labels
     return \@program;
 }
 
-die "usage: perl rxvasm.pl <program.s> <out.bin>" if @ARGV != 2;
+# encodes a stripped program (no labels) into binary form
+sub assemble_binary {
+    # args:
+    # $stripped_program is an arrayref to a program stripped of labels
+    # $label_map is a hashref mapping label names to addresses
+    my ($stripped_program, $label_map) = @_;
+    my @binary;
 
-open my $in, "<", $ARGV[0] or die "Failed to open $ARGV[0]: $!";
+    foreach (@$stripped_program) {
 
-my $program = read_program($in);
+        # encode instructions
+        if (/^([a-z]{2,4})/) {
 
-close $in;
+            # type 'r'
+            push @binary, rxvencode::encode_r($_)
+                if $rxvdef::instruction{$1}{type} eq 'r';
 
-my ($instructions, $label) = rxvlabel::extract_labels($program);
+            # type 'i'
+            push @binary, rxvencode::encode_i($_, $label_map, scalar @binary)
+                if $rxvdef::instruction{$1}{type} eq 'i';
 
-my @binary;
+            next;
+        }
 
-foreach (@$instructions) {
-    if (/^([a-z]{2,4})/) {
-        push @binary, rxvencode::encode_r($_) if $rxvdef::instruction{$1}{type} eq 'r';
-        push @binary, rxvencode::encode_i($_, $label, scalar @binary)
-            if $rxvdef::instruction{$1}{type} eq 'i';
-        next;
+        # encode directives
+        push @binary, @{rxvencode::encode_dir($_)} if (/^\.\w+/);
     }
 
-    push @binary, @{rxvencode::encode_dir($_)} if (/^\.\w+/);
+    # return:
+    # an arrayref to @binary, an array of bytes (machine code)
+    return \@binary;
 }
 
+# ensure rxvasm has been called with 2 arguments, otherwise print an usage
+# message
+die "usage: perl rxvasm.pl <program.s> <out.bin>\n" if @ARGV != 2;
+
+# open and read the input file into $program
+open my $in, "<", $ARGV[0] or die "Failed to open $ARGV[0]: $!";
+my $program = read_program($in);
+close $in;
+
+# first pass: strip and map labels
+my ($stripped_program, $label_map) = rxvlabel::extract_labels($program);
+
+# second pass: parse, encode and assemble the binary
+my $binary = assemble_binary($stripped_program, $label_map);
+
+# write the binary to the output file
 open my $out, '>:raw', $ARGV[1] or die "Failed to open $ARGV[1]: $!";
-print $out pack 'C*', @binary;
+print $out pack 'C*', @$binary;
 close $out;
-
-# debug
-#print "\@instructions:\n";
-#print "$_\n" foreach (@$instructions);
-
-#print "\n\%label:\n";
-#print "label=$_ addr=$label->{$_}\n" foreach (sort keys %$label);
-
-#print "\n\@binary:\n";
-#printf "%08b\n", $_ foreach (@binary);
