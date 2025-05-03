9de25b095df8bca40baa10a83c90be109125e297
phfr24 <phfr24@inf.ufpr.br>
2025-05-03 14:29:25 -0300

    refactor rxvlabel.pl: add comments and organize

--- rxvlabel.pl
+++ rxvlabel.pl
@@ -5,45 +5,50 @@ package rxvlabel;
 use strict;
 use warnings;
 
-# given a trimmed and stripped line (no labels or comments), returns the size
-# in bytes that the instruction or directive needs to be written to the binary
+# compute size in bytes for a line (instruction or directive)
 sub mem_size {
     my ($line) = @_;
 
+    # non-directive lines are assumed to be instructions of size 1 byte
     my ($dir) = $line =~ /^\.(\w+)/ or return 1;
 
+    # bits directive: size = (operand count) x bytes_per_op
     if ($rxvdef::directives{$dir}{type} eq 'bits') {
         my @ops = split /\s+/, $line;
         return (scalar @ops - 1) * $rxvdef::directives{$dir}{bytes_per_op};
     }
 
-    elsif ($rxvdef::directives{$dir}{type} eq 'space') {
-        my ($size) = $line =~ /^\.space\s+(\w+)/;
+    # space directive: operand is size in bytes
+    if ($rxvdef::directives{$dir}{type} eq 'space') {
+        my ($size) = $line =~ /^\.space\s+(\w+)$/;
         return $size;
     }
 
+    # other directives default to zero
     return 0;
 }
 
-# strip labels from \@program and map them to their addresses. returns the
-# references to the stripped program and to the labels map
+# strip labels and build a label to address map
 sub extract_labels {
     my ($program) = @_;
 
-    my @instructions;
-    my %label;
+    my @stripped_program;
+    my %label_map;
     my $addr = 0;
 
     foreach (@$program) {
-        $label{$1} = $addr if (s/^(\w+):\s*//);
 
+        # capture and remove a label recoding its address
+        $label_map{$1} = $addr if (s/^(\w+):\s*//);
+
+        # discard empty lines and compute the next address
         unless (/^$/) {
-            push @instructions, $_;
+            push @stripped_program, $_;
             $addr += mem_size($_);
         }
     }
 
-    return (\@instructions, \%label);
+    return (\@stripped_program, \%label_map);
 }
 
 1;
